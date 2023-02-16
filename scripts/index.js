cff7a3aedbdbde8ce6101c38c0db3cd796627226
Smallwhitelight <smallwhitelight28@gmail.com>
2023-02-16 16:33:36 +0200

    delete coments

--- scripts/index.js
+++ scripts/index.js
@@ -28,34 +28,3 @@ const finishAnimation = () => {
     })
 }
 
-/*slide.addEventListener('click',()=>{
-    if(slide.classList.contains('event')) {
-        container.classList.toggle('blackTowhite')
-        container.classList.toggle('whiteToblack')
-        slide.classList.add('white')
-        trigger_w.classList.add('next-w')
-        trigger_b.classList.add('next-b')
-        slide.classList.remove('event')
-    }else{
-        container.classList.toggle('blackTowhite')
-        container.classList.toggle('whiteToblack')
-        slide.classList.remove('white')
-        trigger_b.classList.add('next-b-reverse')
-        trigger_w.classList.add('next-w-reverse')
-        slide.classList.add('event')
-        slide.classList.add('finish')
-    }
-    finishAnimation()
-})
-const finishAnimation = () => {
-    slide.addEventListener('animationend', (ev) => {
-        if(slide.classList.contains('finish')){
-            trigger_w.classList.remove('next-w-reverse')
-            trigger_b.classList.remove('next-b-reverse')
-            trigger_w.classList.remove('next-w')
-            trigger_b.classList.remove('next-b')
-        }
-        slide.classList.remove('finish')
-    })
-}*/
-
