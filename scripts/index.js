551bee79c7de55b7541174a490b45a4ac546edc7
Smallwhitelight <smallwhitelight28@gmail.com>
2023-02-16 15:52:44 +0200

    JS change

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,30 @@ const trigger_b = document.querySelector('.trigger-black')
 
 
 slide.addEventListener('click',()=>{
+    container.classList.toggle('whiteToblack')
+    container.classList.toggle('blackTowhite')
+    slide.classList.toggle('white')
+    if(!trigger_b.classList.contains('next-b')) {
+        trigger_w.classList.add('next-w')
+        trigger_b.classList.add('next-b')
+           }else{
+        trigger_b.classList.add('next-b-reverse')
+        trigger_w.classList.add('next-w-reverse')
+           }
+    finishAnimation()
+})
+const finishAnimation = () => {
+    slide.addEventListener('animationend', (ev) => {
+        if(trigger_b.classList.contains('next-b-reverse')){
+            trigger_b.classList.toggle('next-b-reverse')
+            trigger_w.classList.toggle('next-w-reverse')
+            trigger_w.classList.toggle('next-w')
+            trigger_b.classList.toggle('next-b')
+        }
+    })
+}
+
+/*slide.addEventListener('click',()=>{
     if(slide.classList.contains('event')) {
         container.classList.toggle('blackTowhite')
         container.classList.toggle('whiteToblack')
@@ -33,5 +57,5 @@ const finishAnimation = () => {
         }
         slide.classList.remove('finish')
     })
-}
+}*/
 
